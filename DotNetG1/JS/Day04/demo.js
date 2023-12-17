d77051119c15f40ea508cec85063ec8246195f6e
sara <sara.atef001@gmail.com>
2023-12-17 11:31:42 +0200

    day04

---
+++ DotNetG1/JS/Day04/demo.js
@@ -0,0 +1,127 @@
+/**
+ * == Events
+ *  == add attribute // done
+ *  == attach functions
+ *  == addEventLister
+ *================= DOM =================
+  == How to Select Element From Dom
+  == Change Content
+  == Change Style 
+  == attribute
+
+ */
+
+// var element = document.querySelector("div p");
+
+// console.log(element);
+// console.log(elements[0].checked);
+// for (var i = 0; i < elements.length; i++) {
+// 	console.log(elements[i]);
+// }
+/** How To select element from dom
+ * ==================== Methods ===============
+ * == getElementById ==> element | null
+ * == querySelector ==> element | null
+ * == getElementsByClassName ==> collection
+ * == getElementsByTagName ==> collection
+ * == getElementsByName ==> collection
+ * == querySelectorAll ==> collection
+ * ====================== Props ==============
+ *  == firstChild
+ *  == firstElementChild
+ *  == document.body
+ *  == [object].children
+ *  == [object].childNodes
+ *  == document.links
+ *  == document.images
+ *  == document.forms
+ * =====================Change Content ========
+ *    == innerText
+ *    == innerHTML
+ *    == textContent (search)
+ * --------------------inputs-------------------
+ *  == value
+ *  ==Checked
+ * ================== ChangeStyle ===========
+ *  == style Object
+ *  == className
+ *  == classList
+ *    -- add
+ *    -- remove
+ *    -- toggle
+ * ======================Attributes ===============
+ *  attributes
+ * -------------methods-----------
+ *  == getAttribute
+ *  == setAttribute
+ *  == hasAttribute
+ *  == removeAttribute
+ *  == toggleAttribute
+ */
+
+// var h2 = document.getElementById("head2");
+// var link1 = document.links[0];
+// var img1 = document.images[0];
+// function execute() {
+// img1.setAttribute("src", "./Images/2.jpg");
+// console.log(img1.getAttribute("src"));
+// if (link1.hasAttribute("class")) {
+// 	link1.removeAttribute("class");
+// } else {
+// 	link1.setAttribute("class", "jsClass");
+// }
+// link1.removeAttribute("class");
+// console.log(link1.hasAttribute("href"));
+// console.log(link1.getAttribute("id"));
+// h2.setAttribute("class", "jsClass");
+// console.log(h2.getAttribute("name"));
+// }
+// function changeStyle() {
+// 	// console.log(p.className); // get
+// 	p.className += " jsClass";
+// }
+
+// function changeStyle() {
+// 	p.classList.add("jsClass");
+// 	console.log(p.classList);
+// 	p.classList.remove("myP");
+// 	p.classList.toggle("jsClass");
+// }
+// function changeStyle() {
+
+// 	// style object
+// 	p.style.backgroundColor = "#073a17";
+// 	p.style.color = "#fff";
+// 	p.style.padding = "30px";
+// }
+
+// var input = document.getElementById("myInput");
+// var outPut = document.getElementById("outPut");
+// var inputs = document.getElementsByName("choose");
+// function getInputValue() {
+// 	// for (var i = 0; i < inputs.length; i++) {
+// 	// 	if (inputs[i].checked) {
+// 	// 		outPut.innerHTML = inputs[i].value;
+// 	// 	}
+// 	// }
+// 	outPut.innerHTML = input.value;
+// 	// input.value = "Hello";
+// 	// console.log( input.value ); // get
+// }
+
+// var p = document.querySelector(".content");
+
+// function changeContent() {
+// 	// console.log(p.innerText); // get
+// 	// p.innerText = "<h1>Hello From ITI</h1>"; // set
+// 	// p.innerHTML = "<h1>Hello From ITI</h1>"; // set
+// }
+
+// console.log(document.images);
+// var parent = document.getElementById("parent");
+
+// console.log(parent.children);
+// console.log(parent.childNodes);
+// console.log(parent.lastChild);
+// console.log(parent.lastElementChild);
+/** */
